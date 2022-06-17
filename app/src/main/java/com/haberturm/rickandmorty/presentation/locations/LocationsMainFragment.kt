4d76c9e241f6a2bd2ef5fe6867f4ed1266ae8f7a
Anton <ovsyannikov.a@yahoo.com>
2022-06-17 11:38:04 +0200

    recycler view

--- app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainFragment.kt
+++ app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainFragment.kt
@@ -1,6 +1,55 @@
 package com.haberturm.rickandmorty.presentation.locations
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.haberturm.rickandmorty.R
+import com.haberturm.rickandmorty.databinding.FragmentLocationsMainBinding
+import com.haberturm.rickandmorty.presentation.charcters.CharacterListAdapter
+import com.haberturm.rickandmorty.presentation.charcters.CharactersMainViewModel
+import com.haberturm.rickandmorty.presentation.decorators.GridSpacingItemDecoration
 
 class LocationsMainFragment : Fragment() {
+
+    private lateinit var locationsAdapter: LocationListAdapter
+
+    private val viewModel: LocationsMainViewModel by lazy {
+        ViewModelProvider(requireActivity()).get(LocationsMainViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        locationsAdapter = LocationListAdapter(
+            listener = object : LocationListAdapter.ActionClickListener{
+                override fun showDetail(id: Int) {
+                    viewModel.showDetails()
+                }
+            },
+            context = requireContext()
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentLocationsMainBinding.inflate(inflater)
+        binding.locationList.apply {
+            layoutManager = GridLayoutManager(requireContext(),2)
+            adapter = locationsAdapter
+            addItemDecoration(
+                GridSpacingItemDecoration(2,resources.getDimensionPixelSize(R.dimen.small_margin) , true, 0)
+            )
+        }
+        locationsAdapter.submitUpdate(viewModel.list)
+
+        return binding.root
+    }
+
+
 }
--- app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainViewModel.kt
+++ app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainViewModel.kt
@@ -1,6 +1,50 @@
 package com.haberturm.rickandmorty.presentation.locations
 
 import androidx.lifecycle.ViewModel
+import com.haberturm.rickandmorty.entities.LocationUi
 
 class LocationsMainViewModel : ViewModel() {
+
+    fun showDetails(){
+
+    }
+
+    val list = listOf<LocationUi>(
+        LocationUi(
+            1,
+            "Citadels of Ricks",
+            "Space station",
+            "unknown",
+        ),
+        LocationUi(
+            2,
+            "Citadels of Ricks",
+            "Space station",
+            "unknown",
+        ),
+        LocationUi(
+            3,
+            "Citadels of Ricks",
+            "Space station",
+            "unknown",
+        ),
+        LocationUi(
+            4,
+            "Citadels of Ricks",
+            "Space station",
+            "unknown",
+        ),
+        LocationUi(
+            5,
+            "Citadels of Ricks",
+            "Space station",
+            "unknown",
+        ),
+        LocationUi(
+            6,
+            "Citadels of Ricks",
+            "Space station",
+            "unknown",
+        )
+    )
 }
