0a4660ea78a90773b60b532eaf32e460ecf1237a
Anton <ovsyannikov.a@yahoo.com>
2022-06-17 10:58:50 +0200

    location list item

---
+++ app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainFragment.kt
@@ -0,0 +1,6 @@
+package com.haberturm.rickandmorty.presentation.locations
+
+import androidx.fragment.app.Fragment
+
+class LocationsMainFragment : Fragment() {
+}
---
+++ app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainViewModel.kt
@@ -0,0 +1,6 @@
+package com.haberturm.rickandmorty.presentation.locations
+
+import androidx.lifecycle.ViewModel
+
+class LocationsMainViewModel : ViewModel() {
+}
