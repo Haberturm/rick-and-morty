c2e517d162ab8aa1b953e247954a83b8a70eae4d
Anton <ovsyannikov.a@yahoo.com>
2022-06-22 11:16:27 +0200

    display location remote data

--- app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainFragment.kt
+++ app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainFragment.kt
@@ -1,16 +1,17 @@
 package com.haberturm.rickandmorty.presentation.locations
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.haberturm.rickandmorty.R
 import com.haberturm.rickandmorty.databinding.FragmentLocationsMainBinding
 import com.haberturm.rickandmorty.di.viewModel.ViewModelFactory
-import com.haberturm.rickandmorty.presentation.charcters.CharactersMainViewModel
+import com.haberturm.rickandmorty.presentation.common.UiState
 import com.haberturm.rickandmorty.presentation.decorators.GridSpacingItemDecoration
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
@@ -36,6 +37,7 @@ class LocationsMainFragment : DaggerFragment() {
             },
             context = requireContext()
         )
+        viewModel.getData()
     }
 
     override fun onCreateView(
@@ -51,7 +53,22 @@ class LocationsMainFragment : DaggerFragment() {
                 GridSpacingItemDecoration(2,resources.getDimensionPixelSize(R.dimen.default_margin) , true, 0)
             )
         }
-        locationsAdapter.submitUpdate(viewModel.list)
+        viewModel.uiState.observe(viewLifecycleOwner, Observer { state ->
+            if (state != null) {
+                when (state) {
+                    UiState.Loading -> {
+                        //todo
+                    }
+                    is UiState.Error -> {
+                        Log.i("DATA", state.exception.toString())
+                    }
+                    is UiState.Data -> {
+                        Log.i("DATA", state.data.toString())
+                        locationsAdapter.submitUpdate(state.data)
+                    }
+                }
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainViewModel.kt
+++ app/src/main/java/com/haberturm/rickandmorty/presentation/locations/LocationsMainViewModel.kt
@@ -4,11 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.haberturm.rickandmorty.domain.common.ApiState
+import com.haberturm.rickandmorty.domain.entities.characters.Characters
+import com.haberturm.rickandmorty.domain.entities.locations.Locations
 import com.haberturm.rickandmorty.domain.repositories.Repository
 import com.haberturm.rickandmorty.presentation.common.UiState
 import com.haberturm.rickandmorty.presentation.entities.CharacterUi
 import com.haberturm.rickandmorty.presentation.entities.LocationUi
+import com.haberturm.rickandmorty.presentation.mappers.characters.CharactersUiMapper
+import com.haberturm.rickandmorty.presentation.mappers.locations.LocationsUiMapper
 import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
 class LocationsMainViewModel @Inject constructor(
@@ -23,6 +29,16 @@ class LocationsMainViewModel @Inject constructor(
     fun getData(){
         viewModelScope.launch {
             _uiState.postValue(UiState.Loading)
+            when(val data = repository.getLocations()){
+                is ApiState.Success<Locations> -> {
+                    _uiState.postValue(
+                        UiState.Data(LocationsUiMapper().fromDomainToUi<Locations, List<LocationUi>>(data.data))
+                    )
+                }
+                is ApiState.Error -> {
+                    UiState.Error(Exception(data.exception))
+                }
+            }
         }
     }
 
